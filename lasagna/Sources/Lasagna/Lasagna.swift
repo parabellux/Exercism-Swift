08090b9468fd42c68f5592fcdef20527db3d7586
parabellux <greta-maria@hotmail.de>
2022-10-09 13:11:05 +0200

    lasagna

---
+++ lasagna/Sources/Lasagna/Lasagna.swift
@@ -0,0 +1,11 @@
+// TODO: define the 'expectedMinutesInOven' constant
+let expectedMinutesInOven = 40
+
+// TODO: define the 'remainingMinutesInOven' function
+let elapsedMinutes = 30 
+func remainingMinutesInOven(expextedMinutesInOven, elapsedMinutes) {
+    return expectedMinutesInOven - elapsedMinutes
+}
+// TODO: define the 'preparationTimeInMinutes' function
+
+// TODO: define the 'totalTimeInMinutes' function
